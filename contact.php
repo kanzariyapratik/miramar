51013e8419fbdfeac6b166ff8ffff79c81bb4756
kanzariyapratik <88300446+kanzariyapratik@users.noreply.github.com>
2021-12-08 10:10:12 +0530

    Initial commit

---
+++ contact.php
@@ -0,0 +1,61 @@
+<!-- Contact -->
+<h1>Contact Us</h1>
+  <div class="contact">
+    <div class="contact_container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="section_title text-center">
+              <div>Ciao</div>
+              <h1>Say Hello</h1>
+            </div>
+            <div class="contact_text text-center">
+              <p><b>It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a semblance of a non-nurturing image of the main driver in one's vehicle. Until then, the lion needs to be tortured, it needs to be aligned with the arrows.</b></p>
+            </div>
+            <div class="contact_form_container">
+              <form action="#" class="contact_form text-center">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <input type="text" class="contact_input" placeholder="Your name" required="required">
+                  </div>
+                  <div class="col-lg-6">
+                    <input type="email" class="contact_input" placeholder="Your email" required="required">
+                  </div>
+                </div>
+                <input type="text" class="contact_input" placeholder="Subject">
+                <textarea class="contact_input" placeholder="Message" required="required"></textarea>
+                <button class="contact_button">send message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <div class="contact_map_container">
+      <div class="map">
+        <div id="google_map" class="google_map">
+          <div class="map_container">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Contact Map Content -->
+      <div class="contact_map_content">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+          <img class="contact_info_logo_1" src="images/logo_4.png" alt="">
+          <img class="contact_info_logo_2" src="images/logo_2.png" alt="">
+          <div class="contact_info_list">
+            <ul class="text-center">
+              <li>132 Liberty palace, near new library,goa/india</li>
+              <li>hello@home.com</li>
+              <li>(+91)9016591910</li>
+			  <li>9054153436</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ home.php
@@ -0,0 +1,336 @@
+<div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Welcome</div>
+            <h1><b>Amazing Hotel In Front Of The Sea</b></h1>
+          </div>
+        </div>
+      </div>
+      <div class="row intro_row">
+        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+          <div class="intro_text text-center">
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div class="row gallery_row">
+        <div class="col">
+
+          <!-- Gallery -->
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">
+              
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_4.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_right container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6 order-xl-1 order-2">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- Rooms Content -->
+        <div class="col-xl-6 order-xl-2 order-1">
+          <div class="rooms_right_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Luxury Double Suite</h1>
+            </div>
+            <div class="rooms_text">
+              <p>It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a semblance of a non-nurturing image of the main driver in one's vehicle. Until then, the lion needs to be tortured, it needs to be aligned with the arrows. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci prount maximus. It was a time for the disease to be unified, but for the kids.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>It was a time of sadness</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>The time was incredibly sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Sticky or not to the borders of the elite</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">Rs: 1499/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_left container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+        
+        <!-- Rooms Content -->
+        <div class="col-xl-6">
+          <div class="rooms_left_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Luxury Single Room</h1>
+            </div>
+            <div class="rooms_text">
+              <p>It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a semblance of a non-nurturing image of the main driver in one's vehicle. Until then, the lion needs to be tortured, it needs to be aligned with the arrows. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci prount maximus. It was a time for the disease to be unified, but for the kids.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>It was a time of sadness</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>The time was incredibly sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Sticky or not to the borders of the elite</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price"> Rs: 849/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover -->
+
+  <div class="discover">
+
+    <!-- Discover Content -->
+    <div class="discover_content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <div class="section_title">
+              <div><b>Hotel</div>
+              <h1>Discover Marimar Hotel</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row discover_row">
+          <div class="col-lg-5">
+            <div class="discover_highlight">
+              <p>It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a salad but not the main hardware.</p>
+            </div>
+            <!-- <div class="button discover_button"><a href="#">discover</a></div> -->
+          </div>
+          <div class="col-lg-7">
+            <div class="discover_text">
+              <p><b>It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a semblance of a non-nurturing image of the main driver in one's vehicle. Until then, the lion needs to be tortured, it needs to be aligned with the arrows. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci prount maximus. It was a time for the disease to be unified, but for the kids. Donec pharetra mattis. There's a lot of pain in it. It was a time for the disease to be unified, but for the kids. There's a lot of pain in it. Each one needs a semblance of a non-nurturing image of the main driver in one's vehicle.</b></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Discover Slider -->
+    <div class="discover_slider_container">
+      <div class="owl-carousel owl-theme discover_slider">
+        
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Weddings</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Parties</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Relax</a></h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Testimonials -->
+
+  <div class="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Clients</div>
+            <h1><b>: Opinions :</b></h1>
+          </div>
+        </div>
+      </div>
+      <div class="row testimonials_row">
+        <div class="col">
+          
+          <!-- Testimonials Slider -->
+          <div class="testimonials_slider_container">
+            <div class="owl-carousel owl-theme testimonials_slider">
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p><b>We vistited this hotel the last mont. The staff were very great and quality sevice, so, the eat is varity and excelent , also the rooms were cleand and confort bed. it has afantastic location for the people that visit the towon for work. Will recomendet thes hotel for all persons that to travel this cityThe best hotel I’ve ever been privileged enough to stay at. Gorgeous building, and it only gets more breathtaking when you walk in. High quality rooms (there was even a tv by the shower), and high quality service. Also, they are one of few hotels that allow people under 21 to book a reservation.</b></p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Kuldip Mathukiya,</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p><b>Great location, really pleasant and clean rooms, but the thing that makes this such a good place to stay are the staff. All of the people are incredibly helpful and generous with their time and advice. We travelled with two six year olds and lots of luggage and despite the stairs up to the elevator this was one of the nicest places we stayed in the four weeks we were in hotel.</b></p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_2.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Bhatt Happy,</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p><b>Staff were all very helpful. Food was great! Room was very clean and comfortable. Very luxiorous hotel. Highly recommendMy 3rd stay here. All staff are very friendly as usual, yet very professional.</b></p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_3.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Jaymit Kanzariya,</a><span> Client</span></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
